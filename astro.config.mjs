23ff42e8fd422fd1a9752c8055f0d8d137417628
Ivan Pozdnovskii <ipozdnovskii@icloud.com>
2025-07-31 13:14:27 +0300

    First deploy 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import sanity from "@sanity/astro";
-import react from "@astrojs/react";
-
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [
-    sanity({
-      projectId: "hz4n1dio",
-      dataset: "production",
-      useCdn: false, // See note on using the CDN
-      apiVersion: "2025-07-26", // insert the current date to access the latest version of the API
-      studioBasePath: "/studio", // If you want to access the Studio on a route
-    }),
-    react(),
-  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,4 @@
-/// <reference types="astro/client" />
-/// <reference types="@sanity/astro/module" />
-
 interface ImportMetaEnv {
-  readonly PUBLIC_SANITY_PROJECT_ID: string;
-  readonly PUBLIC_SANITY_DATASET: string;
-
   readonly SUPABASE_URL: string;
   readonly SUPABASE_ANON_KEY: string;
 }
