3020a292fca6bb4be3a4d84c49bdefa0ecfabd5a
Ivan Pozdnovskii <ipozdnovskii@icloud.com>
2025-08-02 12:15:04 +0300

    Add filters 6

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,22 +7,42 @@ export const server = {
     input: z.object({
       brand: z.string().optional(),
       engine: z.string().optional(),
+      body: z.string().optional(),
+      slug: z.string().optional(),
+      image_src: z.string().optional(),
+      offset: z.number().default(0), // новый параметр
+      limit: z.number().default(15), // новый параметр
     }),
-    handler: async ({ brand = "", engine = "" }) => {
+    handler: async ({
+      brand = "",
+      engine = "",
+      body = "",
+      offset = 0,
+      limit = 11,
+    }) => {
       let query = supabase
         .from("vehicles")
-        .select("id, brand, model, body, engine");
+        .select("id, brand, model, body, engine, slug, image_src", {
+          count: "exact",
+        })
+        .not("image_src", "is", null)
+        .range(offset, offset + limit - 1); // пагинация в Supabase
 
       if (brand) query = query.eq("brand", brand);
       if (engine) query = query.eq("engine", engine);
+      if (body) query = query.eq("body", body);
 
-      const { data, error } = await query;
+      const { data, error, count } = await query;
 
       if (error) {
         throw new Error(error.message);
       }
 
-      return data;
+      return {
+        cars: data || [],
+        total: count || 0,
+        hasMore: offset + limit < (count || 0),
+      };
     },
   }),
 };
